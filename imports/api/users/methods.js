ec79fbe9b62846ce03741c7f6630c465a0919e80
Gee <geniilsonfernandes@gmail.com>
2026-01-14 11:23:18 -0300

    Remove unnecessary code

--- imports/api/users/methods.js
+++ imports/api/users/methods.js
@@ -1,6 +1,5 @@
 import { Meteor } from 'meteor/meteor';
 import { Accounts } from 'meteor/accounts-base';
-import bcrypt from 'bcrypt';
 
 Meteor.methods({
   'users.create'(email, password) {
@@ -8,12 +7,9 @@ Meteor.methods({
       throw new Meteor.Error('invalid-data', 'Email e senha são obrigatórios');
     }
 
-    const salt = bcrypt.genSaltSync(10);
-    const hash = bcrypt.hashSync(password, salt);
-
     const userId = Accounts.createUser({
       email,
-      password: password,
+      password,
     });
 
     return userId;
